1beb1d8bced38efbc17177baab17093026530bb3
Martin Guillon <martin.guillon@akylas.fr>
2019-05-17 16:16:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[![npm](https://img.shields.io/npm/v/nativescript-observable.svg)](https://www.npmjs.com/package/nativescript-observable)
+[![npm](https://img.shields.io/npm/dt/nativescript-observable.svg?label=npm%20downloads)](https://www.npmjs.com/package/nativescript-observable)
+[![GitHub forks](https://img.shields.io/github/forks/Akylas/nativescript-observable.svg)](https://github.com/Akylas/nativescript-observable/network)
+[![GitHub stars](https://img.shields.io/github/stars/Akylas/nativescript-observable.svg)](https://github.com/Akylas/nativescript-observable/stargazers)
+
+[![NPM](https://nodei.co/npm/nativescript-observable.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/nativescript-observable/)
+
+## Installation
+
+* `tns plugin add nativescript-observable`
+
+Be sure to run a new build after adding plugins to avoid any issues.
+
+---
+
+this is a simple subclass of TNS Observable class which let the observable know when a listener was added or removed.
+Very useful to create native object to listen for native event only when necessary
---
+++ src/observable.ts
@@ -0,0 +1,30 @@
+
+import { EventData, Observable } from 'tns-core-modules/data/observable';
+declare module 'tns-core-modules/data/observable' {
+    interface Observable {
+        _getEventList(eventName: string, createIfNeeded?: boolean): any[];
+    }
+}
+
+export class CustomObservable extends Observable {
+    onListenerAdded(eventName: string, count: number) {}
+    onListenerRemoved(eventName: string, count: number) {}
+    public addEventListener(eventNames: string, callback: (data: EventData) => void, thisArg?: Object) {
+        super.addEventListener(eventNames, callback, thisArg);
+        const events = eventNames.split(',');
+        for (let i = 0, l = events.length; i < l; i++) {
+            const event = events[i].trim();
+            const list = this._getEventList(event);
+            this.onListenerAdded(event, list ? list.length : 0);
+        }
+    }
+    public removeEventListener(eventNames: string, callback?: any, thisArg?: Object) {
+        super.removeEventListener(eventNames, callback, thisArg);
+        const events = eventNames.split(',');
+        for (let i = 0, l = events.length; i < l; i++) {
+            const event = events[i].trim();
+            const list = this._getEventList(event);
+            this.onListenerRemoved(event, list ? list.length : 0);
+        }
+    }
+}
